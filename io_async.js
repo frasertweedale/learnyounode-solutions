7dbc7b4f0af575c5410be31ec4aa0e17bffbbdc3
Tram Lai <tramlaisf@gmail.com>
2014-02-10 11:25:14 -0800

    Update io_async.js
    number of newlines need to subtract 1

--- io_async.js
+++ io_async.js
@@ -3,5 +3,11 @@ var fs = require('fs');
 var filename = process.argv[2];
 
 file = fs.readFile(filename, function(err, data) {
-  console.log(data.toString().split('\n').length);
+  console.log(data.toString().split('\n').length - 1 );
 });
+
+//OR
+//fs.readFile(filename, 'utf-8', function(err,data){
+// var newline = data.split('\n');
+// console.log(newline.length - 1)
+//});
