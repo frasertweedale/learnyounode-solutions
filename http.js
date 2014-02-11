1de2bb4144dc12c193773b66b365ea818c2a7d5b
Tram Lai <tramlaisf@gmail.com>
2014-02-11 14:33:47 -0800

    Alternative solution without using setEncoding
    without using setEncoding, data is a Buffer object, hence it needs to be converted to a string prior to console.log

--- http.js
+++ http.js
@@ -8,3 +8,12 @@ http.get(url, function(request) {
     console.log(data);
   });
 });
+
+//OR:
+// var http = require('http');
+// var url = process.argv[2];
+// http.get(url, function(response){
+//   response.on('data', function(data){
+//     console.log(data.toString());
+//   })
+// });
