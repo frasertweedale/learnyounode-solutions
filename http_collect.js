b0dd0b968f11dbcd1d306673fdd8a481112cac20
Tram Lai <tramlaisf@gmail.com>
2014-02-11 14:56:54 -0800

    Alternative solution using third party package

--- http_collect.js
+++ http_collect.js
@@ -13,3 +13,16 @@ http.get(url, function(request) {
     console.log(result);
   });
 });
+
+ //var http = require('http')
+ //  var bl = require('bl')
+  
+ //  http.get(process.argv[2], function (response) {
+ //    response.pipe(bl(function (err, data) {
+ //      if (err)
+ //        return console.error(data)
+ //      data = data.toString()
+ //      console.log(data.length)
+ //      console.log(data)
+ //    }))  
+ //  })
