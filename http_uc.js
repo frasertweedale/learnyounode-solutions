cc1fc376c06a3cb67d4d1f3451defa540f658acf
Tram Lai <tramlaisf@gmail.com>
2014-02-12 02:23:00 -0800

    Alternative solution using third party package

--- http_uc.js
+++ http_uc.js
@@ -12,3 +12,20 @@ server = http.createServer(function(request, response) {
   }
 });
 server.listen(8000);
+
+
+//OR: 
+// var http = require('http');
+// server = http.createServer(function(req,res){
+//   if(req.method != 'POST'){
+//    return res.end('send me a POST\n');
+//    };
+//   var string = '';
+//   req.on('data',function(data){
+//     string += data.toString();
+//   })
+//   req.on('end',function(){
+//     res.end(string.toUpperCase());
+//   });
+// });
+// server.listen(8000);
